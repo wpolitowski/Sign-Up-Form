65110c0543a715db2f737f464e1b95079931e144
wpolitowski <wojciechpolitowski@protonmail.ch>
2022-09-14 13:09:08 +0200

    Add password validation, credits for the photo and delete unnecessary images

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,34 @@
-const email = document.querySelector('input[type="email"]');
+const regexInputs = document.querySelectorAll('input[type="password"], input[type="email"]');
 
-email.addEventListener('change', () => {
+regexInputs.forEach(input => input.addEventListener('change', () => {
     //first email validation is performed after user leaves the input (lazy behaviour)
-    validateEmail();
-    email.addEventListener('input', () => {
+    validateInput(input);
+    input.addEventListener('input', () => {
         //if user needs to correct the email address, validation is performed after each typed character and error is thrown until correct email is typed (aggressive behaviour)
-        validateEmail();
+        validateInput(input);
     });
-});
+}));
 
-function validateEmail() {
-    const regex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-        if (email.value.match(regex) == null) {
-            email.classList.add("invalid");
-        } else email.classList.remove("invalid");
+function validateInput(input) {
+    if (input.type === 'email') {
+        const regex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        if (input.value.match(regex) == null) {
+            input.classList.add("invalid");
+        } else input.classList.remove("invalid");
+    } else if (input.type === 'password') {
+        const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[`!@#$%^&*()_+\-=\[\]{};':\x22\\|,.\x3c>\/?~])[A-Za-z\d`!@#$%^&*()_+\-=\[\]{};':\x22\\|,.\x3c>\/?~]{8,}$/;
+        if (input.value.match(regex) == null) {
+            input.classList.add("invalid");
+        } else input.classList.remove("invalid");
+
+        // const wrongPwdSpan = document.querySelector('input#pwd-confirm + span');
+        if (pwdInputs[0].value !== pwdInputs[1].value && 
+            (pwdInputs[0].value || pwdInputs[0].classList.value.includes('invalid')) &&
+            (pwdInputs[1].value || pwdInputs[1].classList.value.includes('different')) ) {
+            pwdInputs[1].classList.add("different");
+        } else pwdInputs[1].classList.remove("different");
+    }    
 }
+
+const pwdInputs = Array.from(regexInputs).filter(input => input.type === 'password');
+
